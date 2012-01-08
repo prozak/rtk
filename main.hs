1d5c2a5dacec18dc37a21a77fce046695239df93
eddy <eddy@quark.DIR615>
2012-01-09 01:45:38 +0200

    Added command line argument for grammar selection. Moved test grammars to test-grammars/ .

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import System.IO(hGetContents, openFile, IOMode(ReadMode), hClose)
+import System.Environment(getArgs)
 import Control.Exception(bracket)
 import Lexer
 import Parser
@@ -11,24 +12,32 @@ import GenY
 
 import Language.Haskell.TH
 
-main = bracket (openFile "grammar.pg" ReadMode) (hClose)
-    (\hndl ->
-       do
-          content <- hGetContents hndl
-          let grammar = parse . alexScanTokens $ content
---          let grammar = emitLoopsInGrammar . annotateGrammarWithNames . addStartRule . parse . alexScanTokens $ content
---          let grammar = annotateGrammarWithNames . addStartRule . parse . alexScanTokens $ content
---          generateASTFile "AST" grammar
---          generateQQFile "Quote" grammar
-          let grammar0 = normalizeStringLiterals $ addDefaults grammar
-          putStrLn "------ before noralization ------"
-          putStrLn $ showGrammar grammar0
-          let grammar1 = fillConstructorNames $ normalizeTopLevelClauses grammar0
-          putStrLn "------ after noralization ------"
-          putStrLn $ showGrammar grammar1
-          putStrLn $ genY grammar1)
---          putStrLn $ ppShow grammar1)
---          putStrLn $ (generateParserSpec grammar))
+getGrammarFileName = do
+    args <- getArgs
+    return $ case args of
+                s:_ -> s
+                [] -> error $ "Usage: <file>"
+
+-- TODO: failed to use bracket here, options parsing etc
+main = do
+    file <- getGrammarFileName
+    hndl <- (openFile file ReadMode)
+    content <- hGetContents hndl
+    let grammar = parse . alexScanTokens $ content
+--    let grammar = emitLoopsInGrammar . annotateGrammarWithNames . addStartRule . parse . alexScanTokens $ content
+--    let grammar = annotateGrammarWithNames . addStartRule . parse . alexScanTokens $ content
+--    generateASTFile "AST" grammar
+--    generateQQFile "Quote" grammar
+    let grammar0 = normalizeStringLiterals $ addDefaults grammar
+--    putStrLn "------ before noralization ------"
+--    putStrLn $ showGrammar grammar0
+    let grammar1 = fillConstructorNames $ normalizeTopLevelClauses grammar0
+--    putStrLn "------ after noralization ------"
+--    putStrLn $ showGrammar grammar1
+    putStrLn $ genY grammar1
+    hClose hndl
+--    putStrLn $ ppShow grammar1)
+--    putStrLn $ (generateParserSpec grammar))
 
 {--run :: String -> Either String [Token]
 run content = runAlex content $ loop []
